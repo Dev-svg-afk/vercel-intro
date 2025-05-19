7a4d260ae2b54f0b3d532514fbc03828f0c45a67
Dev-svg-afk <24f2003048@ds.study.iitm.ac.in>
2025-05-19 14:44:28 +0530

    testing get_file 3...

--- api/index.py
+++ api/index.py
@@ -3,11 +3,11 @@ import json
 def get_file():
     with open('q-vercel-python.json', 'r') as f:
         data = json.load(f)
-    return data
+    return { "hi" }
 
 def handle_request(names):
     with open('q-vercel-python.json', 'r') as f:
         data = json.load(f)
         a = data[names[0]]
         b = data[names[1]]
-        return { "marks" : [a,b]}
+        return { "marks" : [a,b] }
