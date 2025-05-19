13233a0e9d2fff193bdb98f7d27840897bdeb941
Dev-svg-afk <24f2003048@ds.study.iitm.ac.in>
2025-05-19 15:30:28 +0530

    debugging...

--- api/index.py
+++ api/index.py
@@ -1,12 +1,6 @@
 import json
 import os
 
-def get_file():
-    file_path = os.path.join(os.path.dirname(__file__), 'q-vercel-python.json')
-    with open(file_path, 'r') as f:
-        data = json.load(f)
-    return data
-
 def handle_request(name):
     file_path = os.path.join(os.path.dirname(__file__), 'q-vercel-python.json')
     with open(file_path, 'r') as f:
