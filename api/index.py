7712843d4638d04a92263182224540aebe38559f
Dev-svg-afk <24f2003048@ds.study.iitm.ac.in>
2025-05-19 14:04:48 +0530

    json path fixed

--- api/index.py
+++ api/index.py
@@ -1,12 +1,12 @@
 import json
 
 def get_file():
-    with open('data.json', 'r') as f:
+    with open('q-vercel-python.json', 'r') as f:
         data = json.load(f)
     print(data)
 
 def handle_request(names):
-    with open('data.json', 'r') as f:
+    with open('q-vercel-python.json', 'r') as f:
         data = json.load(f)
         a = data[names[0]]
         b = data[names[1]]
