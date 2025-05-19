0e58b5d65fa1f7d2b78d9398e37604c45f4f4f30
Dev-svg-afk <24f2003048@ds.study.iitm.ac.in>
2025-05-19 15:01:50 +0530

    fixing handle_requests...

--- api/index.py
+++ api/index.py
@@ -8,10 +8,10 @@ def get_file():
     return data
 
 def handle_request(name):
-    file_path = os.path.join(os.path.dirname(__file__), 'q-vercel-python.json')
-    with open(file_path, 'r') as f:
-        data = json.load(f)
-    a = data[name[0]]
-    b = data[name[1]]
+    # file_path = os.path.join(os.path.dirname(__file__), 'q-vercel-python.json')
+    # with open(file_path, 'r') as f:
+    #     data = json.load(f)
+    # a = data[name[0]]
+    # b = data[name[1]]
     # return { "marks" : [a,b] }
     return { name[0] }
